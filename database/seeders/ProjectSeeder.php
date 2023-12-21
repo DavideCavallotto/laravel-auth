052ceb006633bb8c8c5bbfc015a19bd19196d680
DavideCavallotto <davidecavallotto92@gmail.com>
2023-12-21 17:42:41 +0100

    ADD new project in db

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,32 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $projects = 
+        [
+            [
+                'title'=> 'Progetto n1',
+                'description'=>'Il mio progetto numero 1 è molto bello'
+
+            ],
+            [
+                'title'=> 'Progetto n2',
+                'description'=>'Il mio progetto numero 2 è molto bello'
+            ],
+            [
+                'title'=> 'Progetto n3',
+                'description'=>'Il mio progetto numero 3 è molto bello'
+            ]
+
+        ];
+
+        foreach ($projects as $project) {
+            $new_project = new Project();
+
+            $new_project->title = $project['title'];
+            $new_project->description = $project['description'];
+
+            $new_project->save();
+
+        }
     }
 }
